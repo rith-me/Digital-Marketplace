48adb8a038ae46a5518d59c02e11340cac430ab7
rith-me <gbol7415@gmail.com>
2025-03-01 23:27:02 +0700

    fist commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -41,4 +41,25 @@ export const PRODUCT_CATEGORIES = [
       },
     ],
   },
+  {
+    label: 'Softwares',
+    value: 'softwares' as const,
+    featured: [
+      {
+        name: 'Favorite Icon Picks',
+        href: `/products?category=icons`,
+        imageSrc: '/nav/icons/picks.jpg',
+      },
+      {
+        name: 'New Arrivals',
+        href: '/products?category=icons&sort=desc',
+        imageSrc: '/nav/icons/new.jpg',
+      },
+      {
+        name: 'Bestselling Icons',
+        href: '/products?category=icons',
+        imageSrc: '/nav/icons/bestsellers.jpg',
+      },
+    ],
+  },
 ]
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,18 +1,18 @@
-import { buildConfig } from 'payload/config'
-import { webpackBundler } from '@payloadcms/bundler-webpack'
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
-import { slateEditor } from '@payloadcms/richtext-slate'
-import path from 'path'
-import { Users } from './collections/Users'
-import dotenv from 'dotenv'
-import { Products } from './collections/Products/Products'
-import { Media } from './collections/Media'
-import { ProductFiles } from './collections/ProductFile'
-import { Orders } from './collections/Orders'
+import { buildConfig } from 'payload/config';
+import { webpackBundler } from '@payloadcms/bundler-webpack';
+import { mongooseAdapter } from '@payloadcms/db-mongodb';
+import { slateEditor } from '@payloadcms/richtext-slate';
+import path from 'path';
+import { Users } from './collections/Users';
+import dotenv from 'dotenv';
+import { Products } from './collections/Products/Products';
+import { Media } from './collections/Media';
+import { ProductFiles } from './collections/ProductFile';
+import { Orders } from './collections/Orders';
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
-})
+});
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
@@ -32,11 +32,11 @@ export default buildConfig({
   rateLimit: {
     max: 2000,
   },
-  editor: slateEditor({}),
+  editor: slateEditor({}) as any, // Type assertion to ensure compatibility
   db: mongooseAdapter({
     url: process.env.MONGODB_URL!,
   }),
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
   },
-})
+});
